2dfa56dae3f5ae6537c9e07ceb04c0c549f573bf
Shubham-711 <shubhamkumavat639@gmail.com>
2026-02-25 15:11:23 +0530

    Update backend main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,13 @@
 import json
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from .db.database import engine, Base
 from .api import routes
 
-# Create Tables in Supabase
+load_dotenv()
+
+
 #Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Smart Security Scanner")
